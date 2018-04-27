5adadf34d450d93c42f52bf9b67a18e8ff955b23
Carlo <carloalessi94@gmail.com>
2018-04-27 18:07:40 +0200

    cleaned notebook, added pd pos tags to word before creating word embeddings

--- src/analize_text.py
+++ src/analize_text.py
@@ -1,5 +1,10 @@
 from paths import *
+import numpy as np
 import pandas as pd
+
+# nltk
+import nltk
+from nltk import word_tokenize
 
 
 # TODO move this where it will be useful
@@ -7,50 +12,54 @@ def get_sentenceID(entityID):
   ''' Gets entityID and returns sentenceID of a sentence that the drug was mentioned in. '''
   return '.'.join(entityID.split('.')[:-1])
 
+if __name__ == '__main__':
+  sentences_df = pd.read_csv(SENTENCE_PATH)
+  entities_df = pd.read_csv(ENTITY_PATH)
+  print (sentences_df.head())
+  print (entities_df.head())
 
-sentences_df = pd.read_csv(SENTENCE_PATH)
-entities_df = pd.read_csv(ENTITY_PATH)
-print (sentences_df.head())
-print (entities_df.head())
+  label_dict = dict()
 
-label_dict = dict()
+  #for index, row in sentences_df.iterrows():
+   # print(row['sentenceText'])
 
-#for index, row in sentences_df.iterrows():
- # print(row['sentenceText'])
+  #iterate over all entities
+  for index, row in entities_df.iterrows():
+    #get the sentence ID to reach the sentence
+    sentenceID = get_sentenceID(row['entityID'])
+    #print(sentenceID)
+    sentence = str(sentences_df.loc[sentences_df.sentenceID==sentenceID]['sentenceText'].values[0])
+    #print(sentence)
 
-#iterate over all entities
-for index, row in entities_df.iterrows():
-  #get the sentence ID to reach the sentence
-  sentenceID = get_sentenceID(row['entityID'])
-  #print(sentenceID)
-  sentence = str(sentences_df.loc[sentences_df.sentenceID==sentenceID]['sentenceText'].values[0])
-  #print(sentence)
+    #skip all entities that have discontinued names
+    if ';' in row['position']:
+      continue
+    position = row['position'].split('-')
+    words = word_tokenize(sentence.lower())
+    entity_words = sentence[int(position[0]):int(position[1]) + 1].lower().split()
 
-  #skip all entities that have discontinued names
-  if ';' in row['position']:
-    continue
-  position = row['position'].split('-')
-  words = sentence.lower().split()
-  entity_words = sentence[int(position[0]):int(position[1]) + 1].lower().split()
-  #print(words)
-  #print(entity_words)
-  
 
-  if sentenceID not in label_dict:
-    labels = ['O' for w in words] #label everything with O
-  else:
-    labels = label_dict[sentenceID] #load previous labels
+    if sentenceID not in label_dict:
+      labels = ['O' for w in words] #label everything with O
+    else:
+      labels = label_dict[sentenceID] #load previous labels
 
-  for l in range(len(labels)):
-    if labels[l] == 'O': #dont touch the labels that are already defined
-      if words[l] in entity_words:
-        #print(entity_words[0], words[l])
-        if words[l] == entity_words[0]:
-          labels[l] = 'B'
-        else:
-          labels[l] = 'I'
-  label_dict[sentenceID] = labels
-  #print(labels)
-  
-print sentences_df.loc[sentences_df.sentenceID =='DDI-DrugBank.d178.s13']['sentenceText'].values
-print label_dict['DDI-DrugBank.d178.s13']
+    for l in range(len(labels)):
+      if labels[l] == 'O': #dont touch the labels that are already defined
+        if words[l] in entity_words:
+          if words[l] == entity_words[0]:
+            labels[l] = 'B'
+          else:
+            labels[l] = 'I'
+    label_dict[sentenceID] = labels
+
+  print sentences_df.loc[sentences_df.sentenceID =='DDI-DrugBank.d178.s13']['sentenceText'].values
+  print label_dict['DDI-DrugBank.d178.s13']
+
+  # save labels to csv
+  label_dict_path = os.path.join(ROOT_DIR, 'Train', 'bio_labels')
+  np.save(label_dict_path, label_dict)
+  read_dictionary = np.load(label_dict_path + '.npy').item()
+
+  print label_dict['DDI-DrugBank.d178.s13']
+  print read_dictionary['DDI-DrugBank.d178.s13']
